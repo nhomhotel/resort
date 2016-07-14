a59cef4b44ad9727467dac7ed232aeed52eb0e8a
AnhDT <anhdt118@gmail.com>
2016-07-15 02:57:37 +0700

    Add filters and hold states in search room action

--- application/controllers/site/Room.php
+++ application/controllers/site/Room.php
@@ -12,6 +12,7 @@ class Room extends MY_Controller {
         redirect(base_url());
     }
 
+    /*
     public function search(){
         $data['encode'] = $this->config->item('encode_id');
         $_location = trim($this->input->get('location'));
@@ -117,6 +118,7 @@ class Room extends MY_Controller {
         $data['temp'] = ('site/room/list_room');
         $this->load->view('site/layout', isset($data) ? ($data) : null);
 }
+    */
 
     function room_detail($id){
         if($id==null){
@@ -260,6 +262,173 @@ class Room extends MY_Controller {
         $amenities = $this->input->get('amenities')?$this->input->get('amenities'):'';
         
         
+    }
+
+    public function search(){
+
+        $data = array();
+        $data['encode'] = $this->config->item('encode_id');
+
+        /* Step 1. Get Parameters */
+
+        if (!empty($_GET)) {
+            $params = $_GET;
+            foreach ($params as $key => $value) {
+                $data[$key] = $value;
+            }
+            if (!empty($data['guests'])) {
+                $data['guest'] = $data['guests'];
+            }
+        }
+
+        /* Step 2. Generate query by filter parameters */
+
+        $this->db->select('post_room.*,house_type.house_type_name,house_type.house_type_id,address.address_detail,room_type.room_type_name');
+        $this->db->from('post_room');
+        $this->db->join('address','address.address_id = post_room.address_id');
+        $this->db->join('room_type','room_type.room_type_id = post_room.room_type');
+        $this->db->join('house_type','house_type.house_type_id = post_room.house_type');
+        $this->db->join('order','order.post_room_id = post_room.post_room_id','left');
+        $this->db->join('user c', 'post_room.user_id = c.user_id', 'left');
+        $this->db->join('role f', 'c.role_id = f.role_id', 'left');
+
+        if(!empty($params)){
+
+            /* Filter Locations */
+
+            if(!empty($params['location'])){
+                $location_parts = explode(',', $params['location']);
+                if(count($location_parts) >= 3){
+                    $this->db->where('lower(address_street_ascii) like \'%'.$location_parts[0].'%\' or '.'lower(district_ascii) like \'%'.$location_parts[0].'%\'');
+                    $this->db->where('lower(provincial_ascii) like \'%'.$location_parts[1].'%\'');
+                    $this->db->where('lower(country_ascii) like \'%'.$location_parts[2].'%\'');
+                } elseif (count($location_parts)==2) {
+                    $this->db->where('lower(address_street_ascii) like \'%'.$location_parts[0].'%\' or '.'lower(provincial_ascii) like \'%'.$location_parts[0].'%\'');
+                    $this->db->where('lower(country_ascii) like \'%'.$location_parts[1].'%\'');
+                } else{
+                    $this->db->where('lower(address_street_ascii) like \'%'.$location_parts[0].'%\' or '.'lower(district_ascii) like \'%'.$location_parts[0].'%\'  or '.'lower(provincial_ascii) like \'%'.$location_parts[0].'%\' or '.'lower(country_ascii) like \'%'.$location_parts[0].'%\'');
+                }
+            }
+
+            /* Filter By Num Guests */
+
+            if(!empty($params['guests'])){
+                $this->db->where('post_room.num_guest >= ', intval($params['guests']));
+            }
+
+            /* Filter By Num Bathrooms */
+
+            if(!empty($params['bedrooms'])){
+                if (intval($params['bedrooms']) < 10) {
+                    $this->db->where('post_room.num_bedroom', intval($params['bedrooms']));
+                } else {
+                    $this->db->where('post_room.num_bedroom >= ', intval($params['bedrooms']));
+                }
+            }
+
+            /* Filter By Num Bathrooms */
+
+            if(!empty($params['bathrooms'])){
+                if (intval($params['bathrooms']) < 10) {
+                    $this->db->where('post_room.num_bathroom', intval($params['bathrooms']));
+                } else {
+                    $this->db->where('post_room.num_bathroom >= ', intval($params['bathrooms']));
+                }
+            }
+
+            /* Filter By Num Beds */
+
+            if(!empty($params['beds'])){
+                if (intval($params['beds']) < 10) {
+                    $this->db->where('post_room.num_bed', intval($params['beds']));
+                } else {
+                    $this->db->where('post_room.num_bed >= ', intval($params['beds']));
+                }
+            }
+
+            /* Filter By Checkin, Checkout day */
+
+            if(!empty($params['checkin']) && !empty($params['checkout'])){
+                $date_parts = explode('/', $params['checkin']);
+                if (count($date_parts) === 3) {
+                    $checkin_day = $date_parts[2] . '-' . $date_parts[1] . '-' . $date_parts[0];
+                }
+                $date_parts = explode('/', $params['checkin']);
+                if (count($date_parts) === 3) {
+                    $checkout_day = $date_parts[2] . '-' . $date_parts[1] . '-' . $date_parts[0];
+                }
+                if (isset($checkin_day) && isset($checkout_day)) {
+                    $this->db->where('(order.checkin >= "' . $checkin_day . '" OR order.checkout <= "' . $checkout_day . '")');
+                }
+            }
+        }
+        $this->db->group_by('post_room.post_room_id');
+        $this->db->limit(100);
+        $this->db->offset(0);
+        $result = $this->db->get();
+        $query = $this->db->last_query();
+
+        $show_query = $this->input->get('show_query');
+        if (!empty($show_query)) {
+            echo $query;
+        }
+
+        $data['total'] = $result->num_rows();
+        $list_room = $result->result();
+        $result->free_result();
+
+        if(count($list_room) > 0){
+            //get room type
+            foreach ($list_room as $line => $room){
+                $data['room_type'][$line] = array($room->house_type_name, $room->house_type_id);
+                $data['amenities'] = !isset($data['amenities'])?explode(',', $room->amenities):array_merge($data['amenities'],array_diff(explode(',', $room->amenities),$data['amenities']));
+                $data['experience'] = !isset($data['experience'])?explode(',', $room->experience):array_merge($data['experience'],array_diff(explode(',', $room->experience),$data['experience']));
+                $data['price_range'][$line] = $room->price_night_vn;
+            }
+            $data['table_amenities']= get_checkbox_manage_table('amenities','Tiện nghi',  $this, 120,$this->Amenities_model->getListAll());
+            $data['table_experiences'] = get_checkbox_manage_table('experiences','Trải nghiệm',  $this, 120,$this->Experience_model->getListAll());
+            $data['list_room'] = $list_room;
+            sort($data['price_range']);
+            $data['price_range_min'] = min($data['price_range']);
+            $data['price_range_max'] = $data['price_range_min']*100;
+            unset($data['price_range']);
+            if($list_room){
+                $data['list_room'] = $list_room;
+            }
+            else{
+                $data['list_room'] = NULL;
+            }
+        }
+        $data['temp'] = ('site/room/list_room');
+        $config['total_rows'] = $data['total'];
+        $config['per_page'] = 4;
+        $config['use_page_numbers'] = TRUE;
+        $config['page_query_string'] = TRUE;
+        $config['query_string_segment'] = 'page';
+        $config['uri_segment'] = 4;
+        $config['full_tag_open'] = '<ul class="pagination">';
+        $config['full_tag_close'] = '</ul>';
+        $config['first_link'] = false;
+        $config['last_link'] = false;
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['prev_link'] = '&laquo';
+        $config['prev_tag_open'] = '<li class="prev">';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_link'] = '&raquo';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="#">';
+        $config['cur_tag_close'] = '</a></li>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+
+        $this->pagination->initialize($config);
+        $data['pagination_link'] = $this->pagination->create_links();
+        $data['per_page'] = $config['per_page'];
+        $this->load->view('site/layout', isset($data) ? ($data) : null);
     }
 }
 ?>
--- public/site/js/starview.js
+++ public/site/js/starview.js
@@ -1,11 +1,11 @@
 //--------------------------------Bookroom---------------------------------------
 
-    $(document).ready(function(){
-        if( $(window).width() > 768){
-            $(function() {
-                var offset = $("#book-right").offset();
-                var bottom = $("#book-main").outerHeight(true) ;
-                var topPadding = 0;
+$(document).ready(function () {
+    if ($(window).width() > 768) {
+        $(function () {
+            var offset = $("#book-right").offset();
+            var bottom = $("#book-main").outerHeight(true);
+            var topPadding = 0;
 //                $(window).scroll(function() {
 //                    alert($(window).scrollTop());
 //                    if ($(window).scrollTop() >= offset.top & $(window).scrollTop() < bottom) {
@@ -25,210 +25,232 @@
 //                    };
 //
 //                });
-            });
-        }
-    });
+        });
+    }
+});
 
 //------------------------------------------Listroom------------------------------------------
-
-$(document).ready(function(){
-    $(function() {
-        $( "#slider-range" ).slider({
-            range: true,
-            min: 0,
-            max: 1000,
-            values: [ 0, 1000 ],
-            slide: function( event, ui ) {
-                $( "#min-amount" ).val( "$" + ui.values[ 0 ] );
-                $( "#max-amount" ).val( "$" + ui.values[ 1 ] );
-            },
-            change:function(ev, ui){
-                console.log(ui.values[0]);
-                console.log(ui.values[1]);
-                console.log(ev);
-            }
-        });
-        $( "#min-amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ));
-        $( "#max-amount" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ));
-    });
-
-    $(function(){
-        $("#filter-control").on("click",function(){
+if (typeof(min_value) == "undefined") {
+    var min_value = 0;
+}
+if (typeof(max_value) == "undefined") {
+    var max_value = 1000;
+}
+$(document).ready(function() {
+    $(function () {
+        $("#filter-control").on("click", function () {
             $("#sidebar").toggleClass("show");
         });
     });
 
-    $(function(){
-        $("#price-sort").on("click",function(){
+    $(function () {
+        $("#price-sort").on("click", function () {
             $("#price-sort").toggleClass("sort-reverse");
         });
     });
-    $(function(){
+    $(function () {
         $('.checkbox input[type="checkbox"]', '.checkbox-inline input[type="checkbox"]').each(function () {
-        $(this).prop("checked") && $(this).parent("label").addClass("checked"), $(this).prop("disabled") && $(this).parent("label").addClass("disabled")
+            $(this).prop("checked") && $(this).parent("label").addClass("checked"), $(this).prop("disabled") && $(this).parent("label").addClass("disabled")
         }), $(document).on("change", '.checkbox input[type="checkbox"], .checkbox-inline input[type="checkbox"]', function () {
             $(this).prop("checked") ? $(this).parent("label").addClass("checked") : $(this).parent("label").removeClass("checked")
         })
     });
-    $(function(){
+    $(function () {
         var room_type = [];
         var amenities = '';
         var experiences = '';
         var bedroom = $('#bedroom');
-        var bathroom =$('#bathroom');
-        var beds =$('#beds');
-        $('.tclick').click(function(){
+        var bathroom = $('#bathroom');
+        var beds = $('#beds');
+        var min = $('#min-amount');
+        var max = $('#max-amount');
+        var guest = $('#guests');
+        $('.tclick').click(function () {
             var currentclick = $(this);
-            if(currentclick.parent().hasClass('book-action')){
+            if (currentclick.parent().hasClass('book-action')) {
                 var checkin = $('#bookin-dpk');
                 var checkout = $('#bookout-dpk');
-                var guest = $('#guests');
                 console.log(typeof $('#name_customer'));
-                if(typeof $('#name_customer')==undefined || $('#name_customer').val()=='' ||typeof $('#phone_number')==undefined || $('#phone_number').val()=='' ||typeof $('#email')==undefined || $('#email').val()=='')
-                {
+                if (typeof $('#name_customer') == undefined || $('#name_customer').val() == '' || typeof $('#phone_number') == undefined || $('#phone_number').val() == '' || typeof $('#email') == undefined || $('#email').val() == '') {
                     $('#myModal').modal('show');
                     return;
                 }
-                if(checkin.val()==''||typeof checkin.val() == undefined){
+                if (checkin.val() == '' || typeof checkin.val() == undefined) {
                     $('.info-book').html('nhập ngày nhận phòng');
                     return;
                 }
-                if(checkout.val()==''||typeof checkout.val() == undefined){
+                if (checkout.val() == '' || typeof checkout.val() == undefined) {
                     $('.info-book').html('nhập ngày trả phòng');
                     return;
                 }
-                if(guest.val()==''||typeof guest.val() == undefined){
+                if (guest.val() == '' || typeof guest.val() == undefined) {
                     $('.info-book').html('nhập số khách');
                     return;
                 }
-                window.location.href = url+'room/order_room/'+id+'?checkin='+checkin.val()+"&checkout="+checkout.val()+"&guests="+guest.val();
-            }
-            else{
-//                if(typeof )
-                var amenities = '';
-                var experiences = '';      
-                $('[name="amenities"]:checked').each(function(){
-                    amenities +=$(this).data('tloc')+',';
-                })
-                $('[name="experiences"]:checked').each(function(){
-                    experiences +=$(this).data('tloc')+',';
-                })
+                var url = url + 'room/order_room/' + id + '?checkin=' + checkin.val() + "&checkout=" + checkout.val() + "&guests=" + guest.val();
+                if (parseInt(min.val().toString().replace("$", "")) > 0) {
+                    url += "&min=" + min;
+                }
+                if (parseInt(max.val().toString().replace("$", "")) > 0) {
+                    url += "&max=" + max;
+                }
+                window.location.href = url;
+            } else {
+                /*
+                 var amenities = '';
+                 var experiences = '';
+                 $('[name="amenities"]:checked').each(function(){
+                 amenities +=$(this).data('tloc')+',';
+                 })
+                 $('[name="experiences"]:checked').each(function(){
+                 experiences +=$(this).data('tloc')+',';
+                 })
 
-                $("#room_type label.tclick ").each(function(index){
-                    var cur = $(this);
-                    data_tloc = cur.data('tloc');
-        //            if(room_type.length==0){room_type.push(data_tloc);}
-        //            else{
-                        var index = room_type.indexOf(data_tloc);
-                        if(index==-1){
-                            room_type.push(data_tloc);
-                        }else{
-                            room_type.splice(index,1);
-                        }
-        //            }
-                })
-//                console.log(amenities);
-//                console.log(experiences);
-//                console.log(bedroom);
-//                console.log(bathroom);
-//                console.log(beds);
-//                console.log($('input#location,input#checkin,input#checkout,input#guest,input#option1,input#price-sort,input#entry-home'));
-                var currentthis = $(this);
-                var room_types = '';
-//                $('#room_type .tclick').each(function(){
-//                    if(currentthis.data('tloc')==$(this).data('tloc')){
-//                         var xxx = $(this).attr("class").split(' ');
-//                         $.each(xxx, function(a,v){
-//                             if(v==='active'){
-//                                 console.log('da active truoc day');
-//                             }
-//                         })
-//                         
-//                    }
-//                    console.log(currentthis.data('tloc'));
-//                    console.log($(this).data('tloc'));
-//                });
-//                return false;
-                var data = {};
-                if(typeof amenities !==undefined && amenities !='')data['amenities'] = amenities;
-                if(typeof experiences !==undefined && experiences !='')data['experiences'] = experiences;
-                if(typeof bedroom !==undefined && $.isNumeric(bedroom.val()))data['bedroom'] = bedroom.val();
-                if(typeof bathroom !==undefined && $.isNumeric(bathroom.val()))data['bathroom'] = bathroom.val();
-                if(typeof beds !==undefined && $.isNumeric(beds.val()))data['beds'] = beds.val();
-//                if(typeof amenities !==undefined && amenities !='')data['amenities'] = amenities;
-//                if(typeof amenities !==undefined && amenities !='')data['amenities'] = amenities;
-                var xhr = $.ajax({
-                    url:url+'room/search',
-                    data:data,
-                    type:'GET',
-                    success:function(data){
-                        console.log(data);
-                    },
-                    error:'',
-                    dataType: 'json',
-                })
+                 $("#room_type label.tclick ").each(function(index){
+                 var cur = $(this);
+                 data_tloc = cur.data('tloc');
+                 if(room_type.length==0){room_type.push(data_tloc);}
+                 else{
+                 var index = room_type.indexOf(data_tloc);
+                 if(index==-1){
+                 room_type.push(data_tloc);
+                 }else{
+                 room_type.splice(index,1);
+                 }
+                 }
+                 })
+                 console.log(amenities);
+                 console.log(experiences);
+                 console.log(bedroom);
+                 console.log(bathroom);
+                 console.log(beds);
+                 console.log($('input#location,input#checkin,input#checkout,input#guest,input#option1,input#price-sort,input#entry-home'));
+                 var currentthis = $(this);
+                 var room_types = '';
+                 $('#room_type .tclick').each(function(){
+                 if(currentthis.data('tloc')==$(this).data('tloc')){
+                 var xxx = $(this).attr("class").split(' ');
+                 $.each(xxx, function(a,v){
+                 if(v==='active'){
+                 console.log('da active truoc day');
+                 }
+                 })
+
+                 }
+                 console.log(currentthis.data('tloc'));
+                 console.log($(this).data('tloc'));
+                 });
+                 return false;
+                 var data = {};
+                 if(typeof amenities !==undefined && amenities !='')data['amenities'] = amenities;
+                 if(typeof experiences !==undefined && experiences !='')data['experiences'] = experiences;
+                 if(typeof bedroom !==undefined && $.isNumeric(bedroom.val()))data['bedroom'] = bedroom.val();
+                 if(typeof bathroom !==undefined && $.isNumeric(bathroom.val()))data['bathroom'] = bathroom.val();
+                 if(typeof beds !==undefined && $.isNumeric(beds.val()))data['beds'] = beds.val();
+                 if(typeof amenities !==undefined && amenities !='')data['amenities'] = amenities;
+                 if(typeof amenities !==undefined && amenities !='')data['amenities'] = amenities;
+                 var xhr = $.ajax({
+                 url:url+'room/search',
+                 data:data,
+                 type:'GET',
+                 success:function(data){
+                 console.log(data);
+                 },
+                 error:'',
+                 dataType: 'json',
+                 })
+                 */
+                var bathrooms = $('#bathrooms');
+                var bedrooms = $('#bedrooms');
+                var guest = $('#guest');
+                var checkin = $('#checkin');
+                var checkout = $('#checkout');
+                var location = $('#location');
+                var url = 'room/search?location=' + encodeURIComponent(location.val()) + '&checkin=' + checkin.val() + "&checkout=" + checkout.val();
+                if (parseInt(guest.val()) > 0) {
+                    url += "&guests=" + guest.val();
+                }
+                if (parseInt(beds.val()) > 0) {
+                    url += "&beds=" + beds.val();
+                }
+                if (parseInt(bathrooms.val()) > 0) {
+                    url += "&bathrooms=" + bathrooms.val();
+                }
+                if (parseInt(bedrooms.val()) > 0) {
+                    url += "&bedrooms=" + bedrooms.val();
+                }
+                min = parseInt(min.val().toString().replace("$", ""));
+                max = parseInt(max.val().toString().replace("$", ""));
+                if (min >= 0) {
+                    url += "&min=" + min;
+                }
+                if (max >= 0) {
+                    url += "&max=" + max;
+                }
+                window.location.href = url;
+                return false;
             }
         })
     });
-    $(function(){
-            $('#language li').click(function(){
-                var language = $(this).find('a');
-                var data = {language:language.data('value')};
+    $(function () {
+        $('#language li').click(function () {
+            var language = $(this).find('a');
+            var data = {language:language.data('value')};
             $('#language').find('.icon-language span').html(language.html());
             var urlCurrent = window.location.href;
             $.ajax({
-                url:url+'language',
-                    dataType: 'json',
-                    success: function (data) {
-                        window.location.href = urlCurrent;
-                    },
-                    data: data,
-                    type: 'POST'
+                url:url + 'language',
+                dataType:'json',
+                success:function (data) {
+                    window.location.href = urlCurrent;
+                },
+                data:data,
+                type:'POST'
             });
         });
-        }); 
+    });
 })
 
 //----------------------------------------------------Index--------------------------------------------
 
-    $(document).ready(function(){
-        //sticky navigation
-        var nav = document.querySelector('#navigation');
-        document.addEventListener('scroll', function(){
-            if(window.scrollY >= 20){
-                nav.classList.add('sticky');
-            }else{
-                nav.classList.remove('sticky');
-            }
-        });
-
-        //wow.js
-        new WOW().init();
-
-
-        //owl-caorousel
-        var owl = $(".owl-carousel");
-        owl.owlCarousel({
-            margin:100,
-            loop:true,
-            autoplay:true,
-            // nav:true,
-            // navText:['<i class="glyphicon glyphicon-menu-left"></i>','<i class="glyphicon glyphicon-menu-right"></i>'],
-            autoplayTimeout:1500,
-            autoplayHoverPause:true,
-            autoplaySpeed: 1000,
-            dotsSpeed: 400,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1,
-                },
-                600:{
-                    items:3,
-                },
-                1000:{
-                    items:3,
-                }
-            }
-        });
+$(document).ready(function () {
+    //sticky navigation
+    var nav = document.querySelector('#navigation');
+    document.addEventListener('scroll', function () {
+        if (window.scrollY >= 20) {
+            nav.classList.add('sticky');
+        } else {
+            nav.classList.remove('sticky');
+        }
     });
+
+    //wow.js
+    new WOW().init();
+
+
+    //owl-caorousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        margin:100,
+        loop:true,
+        autoplay:true,
+        // nav:true,
+        // navText:['<i class="glyphicon glyphicon-menu-left"></i>','<i class="glyphicon glyphicon-menu-right"></i>'],
+        autoplayTimeout:1500,
+        autoplayHoverPause:true,
+        autoplaySpeed:1000,
+        dotsSpeed:400,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+});
