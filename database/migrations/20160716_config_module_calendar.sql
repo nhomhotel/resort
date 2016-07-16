55824fddda247dcb40ba3f1a6e091b44849bddee
AnhDT <anhdt118@gmail.com>
2016-07-16 16:03:40 +0700

    run sql commands to config module calendar

---
+++ database/migrations/20160716_config_module_calendar.sql
@@ -0,0 +1,2 @@
+INSERT INTO `tbl_modules` (`module_id`, `name_lang_key`, `sort`, `active`) VALUES ('Calendar', 'modules_Calendar', 5, 1);
+INSERT INTO `tbl_permissions` (`module_id`, `user_id`) VALUES ('Calendar', 2);
